ab00adbe1045643a22620587304a03718d30d7f4
Ommyjoh <emmanuelboshe@gmail.com>
2022-09-12 18:12:16 +0300

    changes

--- app/Http/Livewire/Admin/Invoice/PrintInvoice.php
+++ app/Http/Livewire/Admin/Invoice/PrintInvoice.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire\Admin\Invoice;
 
+use App\Models\Invoice;
 use Livewire\Component;
 
 class PrintInvoice extends Component
 {
+    public $invoice;
+
+    public function mount(Invoice $invoice){
+        $this->invoice = $invoice->toArray();
+    }
     public function render()
     {
+        dd($this->invoice);
         return view('livewire.admin.invoice.print-invoice');
     }
 }
